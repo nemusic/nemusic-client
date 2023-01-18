ad64351989622a0fa31669d94a54172f6f9bfc33
vovan <vovanshil95@mail.ru>
2023-01-18 16:04:56 +0300

    add edit-playlist page and change samples to js

---
+++ src/pages/EditPlaylistPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import EditPlaylist from '../components/EditPlaylist';
+
+import Page from './Page';
+
+function CollectionsPage() {
+  return (
+    <Page>
+      <EditPlaylist />
+    </Page>
+  );
+}
+
+export default CollectionsPage;
