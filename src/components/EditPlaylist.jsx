d8264d836ee3c416793b321143bb5f003849ade0
vovan <vovanshil95@mail.ru>
2023-01-18 16:06:36 +0300

    add edit-playlist page and change samples to js

--- src/components/EditPlaylist.jsx
+++ src/components/EditPlaylist.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from '../styles/EditPlaylist.module.css';
 import addPhoto from '../resources/add_photo.svg';
+import submitIcon from '../resources/submit_icon.svg';
 
 function EditPlaylist() {
+  const [playlistСover, setPlaylistCover] = useState(addPhoto);
+
+  function addPlaylistCover(event) {
+    setPlaylistCover(URL.createObjectURL(event.target.files[0]));
+  }
+
   return (
     <div>
       <div className={classes.header}>
-        Редактирование плейлиста
+        <div>
+          Редактирование плейлиста
+        </div>
+        <div className={classes.submit__icon}>
+          <a href="playlist">
+            <img src={submitIcon} alt="" />
+          </a>
+        </div>
       </div>
       <div className={classes.edit_container}>
-        <div className={classes.add_photo_form}>
-          <img src={addPhoto} alt="" />
-          <input type="file" id="file-input" src={addPhoto} />
+        <div className={classes.add_photo_form} onClick={() => document.getElementById('file-input').click()}>
+          <div className={classes.mask}>
+            <img src={playlistСover} alt="" />
+            <input type="file" id="file-input" accept="image/*,.png,.jpg,.web" onChange={addPlaylistCover} hidden />
+          </div>
         </div>
         <div>
-          <div className={classes.title}>Название плейлиста</div>
-          <div className={classes.description}>Описание</div>
+          <div>
+            <div contentEditable className={classes.title__conteiner}>
+              <span className={classes.title}>Название плейлиста</span>
+            </div>
+          </div>
+          <div contentEditable className={classes.description__conteiner}>
+            <span className={classes.description}>Описание</span>
+          </div>
         </div>
       </div>
     </div>
