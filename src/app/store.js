f28257f704af0b2366cde63f4aab560735813134
alefunt <sashkak0204@gmail.com>
2023-01-22 04:36:00 +0300

    added redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import playerReducer from '../redux_features/player/player-slice';
 
-export const store = configureStore({
-  reducer: {
-    player: playerReducer
-  }
+const rootReducer = combineReducers({
+  player: playerReducer
 });
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
+});
+
+export const persistor = persistStore(store);
+export default store;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
-import { Provider } from 'react-redux';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store } from './app/store';
+import store, { persistor } from './app/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
