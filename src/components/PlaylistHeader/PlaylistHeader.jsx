7fa1753352eb52de8ef298359f9f1d0b60e75a31
clif2501 <clif2501@gmail.com>
2023-01-23 09:19:21 +0300

    Playlist likes

--- src/components/PlaylistHeader/PlaylistHeader.jsx
+++ src/components/PlaylistHeader/PlaylistHeader.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import editIcon from '../../resources/edit_icon.svg';
+import FavoriteIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteFilledIcon from '@mui/icons-material/Favorite';
+
+import authHeader from '../../services/auth-header';
 
 import classes from './PlaylistHeader.module.css';
 
-function PlaylistHeader({ cover, title, description }) {
+const API_URL = 'http://localhost:8080/api/user/collections';
+
+function PlaylistHeader({
+  id, cover, title, description, liked
+}) {
+  const [likedIcon, setLikedIcon] = useState(liked);
+  useEffect(() => {
+    fetch('http://localhost:8080/api/playlist/'.concat(id), { headers: authHeader() })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setLikedIcon(data.liked);
+      });
+  });
+
+  const handleLike = () => {
+    setLikedIcon(() => !likedIcon);
+    fetch(API_URL.concat('?id=').concat(id).concat('&operation=').concat(likedIcon ? 'dislike' : 'like'), {
+      method: 'POST',
+      headers: authHeader()
+    });
+  };
+
+  const icon = likedIcon ? <FavoriteFilledIcon /> : <FavoriteIcon />;
+
   return (
     <div className={classes.flex_container}>
       <div className={classes.cover}>
@@ -18,10 +46,8 @@ function PlaylistHeader({ cover, title, description }) {
           {description}
         </div>
       </div>
-      <div className={classes.edit_button}>
-        <a href="/edit">
-          <img src={editIcon} alt="" />
-        </a>
+      <div className={classes.edit_button} onClick={handleLike}>
+        {icon}
       </div>
     </div>
   );
