94ba39aea10a18e63ec51477eb3ab23b9944301e
clif2501 <clif2501@gmail.com>
2023-01-23 10:13:23 +0300

    Fix liked tracks

--- src/pages/LikesPage/LikesPage.jsx
+++ src/pages/LikesPage/LikesPage.jsx
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Page from '../Page/Page';
 import TrackList from '../../components/TrackList/TrackList';
+import {
+  isPlayingChanged,
+  playlistChanged,
+  trackChanged
+} from '../../redux_features/player/player-slice';
 
 import authHeader from '../../services/auth-header';
 
 const API_URL = 'http://localhost:8080/api/user/likes';
 
 function LikesPage() {
+  const dispatch = useDispatch();
+  const currentPlaylistId = useSelector((state) => state.player.playlist.id);
+  const currentTrack = useSelector((state) => state.player.current);
+  const isPlaying = useSelector((state) => state.player.isPlaying);
+
   const [tracks, setTracks] = useState([]);
   useEffect(() => {
     fetch(API_URL, { headers: authHeader() })
@@ -17,10 +28,28 @@ function LikesPage() {
       });
   }, []);
 
+  const trackOnClickPlay = (trackId) => {
+    if (currentPlaylistId !== 0) {
+      const tmp = tracks;
+      dispatch(playlistChanged({
+        id: 0,
+        title: 'Избранное',
+        description: '',
+        cover: '',
+        tracks: tmp
+      }));
+    }
+    if (trackId !== currentTrack.id) {
+      dispatch(trackChanged(trackId));
+    } else {
+      dispatch(isPlayingChanged(!isPlaying));
+    }
+  };
+
   return (
     <Page>
       <h1>Понравившиеся треки</h1>
-      <TrackList tracks={tracks} />
+      <TrackList tracks={tracks} onClickPlay={trackOnClickPlay} />
     </Page>
   );
 }
